b3e8c7ae4150032cbc89e2e006f69d33107e4269
Aaron Greenberg <65680737+aaronlearns@users.noreply.github.com>
2022-09-05 21:02:10 -0400

    Upload files

---
+++ main.js
@@ -0,0 +1,35 @@
+// If you're on the "work" page, the "work" nav link shows as selected etc.
+function highlightCurrentPageLink() {
+
+  let navLinks = document.getElementsByClassName("navlink");
+
+  for (let n = 0; n < navLinks.length; n = n + 1) {
+
+    let link = navLinks[n].getAttribute("href");
+    // console.log("Trying for a link at" . n);
+
+    if (window.location.href.includes(link)) {
+      // console.log("Found a link at". n);
+      navLinks[n].classList.add("clicked");
+      navLinks[n].classList.remove("borderhover");
+    } else {
+      // console.log("No link found");
+      continue;
+    }
+  }
+}
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+// Controls the sticky navbar
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.onload = function () {highlightCurrentPageLink(); console.log("Calling highlight function");};
+window.onscroll = function() {stickyNavbar()};
